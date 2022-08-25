bca6b90d03ab1ac08ed5874218f5cce6dc9b20ac
WeeTiong Chia <xiewz78@hotmail.com>
2022-08-25 21:47:09 +0800

    Add register function

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -7,11 +7,11 @@ import {
     TouchableOpacity,
     View,
   } from "react-native";
-  import React, { useState } from "react";
+  import React, { useState, useEffect } from "react";
   import { useNavigation } from "@react-navigation/native";
   import axios from "axios";
   import AsyncStorage from "@react-native-async-storage/async-storage";
-  import { PROFILE_SCREEN, API, API_LOGIN } from "../constants";
+  import { PROFILE_SCREEN, API, API_LOGIN, API_SIGNUP } from "../constants";
   
   export default function AuthScreen() {
     const navigation = useNavigation();
@@ -19,9 +19,39 @@ import {
     const [password, setPassword] = useState("");
     const [errorText, setErrorText] = useState("");
     const [loading, setLoading] = useState(false);
-    // const [isLoginScreen, setIsLoginScreen] = useState(true);
-    // const [confirmPassword, setConfirmPassword] = useState();
+    const [isLoginScreen, setIsLoginScreen] = useState(true);
+    const [confirmPassword, setConfirmPassword] = useState();
   
+    useEffect(() => {
+        return () => setLoading(false);
+    }, []);
+
+    async function signUp() {
+        setLoading(true);
+        if (password != confirmPassword) {
+            setErrorText("Your passwords don't match. Check and try again.");
+        }
+        else {
+            try {
+                const response =await axios.post(API + API_SIGNUP, {
+                    username,
+                    password,
+                });
+                if (response.data.Error) {
+                    // We have an error message for if the user already exists
+                    setErrorText(response.data.Error);
+                }
+                else {
+                    login();
+                }
+            }
+            catch (error) {
+                console.log("Failed logging in, Error: ", error.response);
+                setErrorText(error.response.data.description);
+            }
+        }
+    }
+
     async function login() {
       setLoading(true);
       Keyboard.dismiss();
@@ -40,7 +70,9 @@ import {
     }
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>Login to your account</Text>
+        <Text style={styles.title}>
+            {isLoginScreen ? "Login to your account" : "Register new account"}
+        </Text>
   
         <TextInput
           style={styles.inputView}
@@ -56,31 +88,61 @@ import {
           value={password}
           onChangeText={(pw) => setPassword(pw)}
         />
+
+        {!isLoginScreen && (
+            <View style={styles.inputView}>
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="Password Confirm"
+                    secureTextEntry={true}
+                    onChangeText={(pw) => setConfirmPassword(pw)}
+                    />
+            </View>
+        )}
   
         <TouchableOpacity
           style={styles.button}
           onPress={async () => {
-            await login();
+            isLoginScreen ? await login() : await signUp();
           }}
         >
           {loading ? (
             <ActivityIndicator style={styles.buttonText} />
           ) : (
-            <Text style={styles.buttonText}>Login</Text>
+            <Text style={styles.buttonText}>
+                {isLoginScreen ? "Login" : "Register"}
+            </Text>
           )}
-        </TouchableOpacity>
+        </TouchableOpacity>w
   
+        <TouchableOpacity
+          onPress={() => {
+            setIsLoginScreen(!isLoginScreen);
+            setErrorText("");
+          }}
+        >
+            <Text style={styles.switchText}>
+                {isLoginScreen ? "No account? Sign up now." 
+                : "Already have an account? Log in here."}
+            </Text>
+        </TouchableOpacity>
+
         <Text style={styles.errorText}>{errorText}</Text>
       </View>
     );
   }
   
   const styles = StyleSheet.create({
-    errorText: {
+    switchText: {
       marginTop: 20,
       fontSize: 15,
-      color: "red",
+      color: "gray",
     },
+    errorText: {
+        marginTop: 20,
+        fontSize: 15,
+        color: "red",
+      },
     container: {
       flex: 1,
       backgroundColor: "#fff",
